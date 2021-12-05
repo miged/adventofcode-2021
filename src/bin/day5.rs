67667b32827aa2aa330b67314b77c1ddfa58b59f
Miguel <mige.d@outlook.com>
2021-12-05 16:47:06 -0500

    cleanup day5 using signum

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -6,8 +6,8 @@ pub fn main() {
 
 #[derive(Debug)]
 struct Line {
-    start: (usize, usize),
-    end: (usize, usize),
+    start: (i32, i32),
+    end: (i32, i32),
 }
 
 fn parse_file() -> Vec<Line> {
@@ -22,8 +22,8 @@ fn parse_file() -> Vec<Line> {
             .split_ascii_whitespace()
             .map(|i| i.parse().unwrap())
             .collect();
-        let tup1: Vec<usize> = split[0].split(',').map(|i| i.parse().unwrap()).collect();
-        let tup2: Vec<usize> = split[2].split(',').map(|i| i.parse().unwrap()).collect();
+        let tup1: Vec<i32> = split[0].split(',').map(|i| i.parse().unwrap()).collect();
+        let tup2: Vec<i32> = split[2].split(',').map(|i| i.parse().unwrap()).collect();
         entries.push(Line {
             start: (tup1[0], tup1[1]),
             end: (tup2[0], tup2[1]),
@@ -34,34 +34,26 @@ fn parse_file() -> Vec<Line> {
 }
 
 fn solve(lines: &mut [Line], ignore_diagonals: bool) -> usize {
-    let mut grid: Vec<Vec<usize>> = vec![vec![0; 999]; 999];
-
+    let mut grid: Vec<Vec<i32>> = vec![vec![0; 999]; 999];
     for line in lines {
         if line.start.0 != line.end.0 && line.start.1 != line.end.1 {
             // diagonal line
             if !ignore_diagonals {
                 for _ in line.start.1.min(line.end.1)..line.start.1.max(line.end.1) + 1 {
-                    grid[line.start.1][line.start.0] += 1;
-
-                    match line.start.0 < line.end.0 {
-                        true => line.start.0 += 1,
-                        false => line.start.0 -= 1,
-                    }
-                    match line.start.1 < line.end.1 {
-                        true => line.start.1 += 1,
-                        false => line.start.1 -= 1,
-                    }
+                    grid[line.start.1 as usize][line.start.0 as usize] += 1;
+                    line.start.0 += (line.end.0 - line.start.0).signum();
+                    line.start.1 += (line.end.1 - line.start.1).signum();
                 }
             }
         } else if line.start.0 == line.end.0 {
             // vertical line
             for i in line.start.1.min(line.end.1)..line.start.1.max(line.end.1) + 1 {
-                grid[i][line.start.0] += 1;
+                grid[i as usize][line.start.0 as usize] += 1;
             }
         } else {
             // horizontal line
             for i in line.start.0.min(line.end.0)..line.start.0.max(line.end.0) + 1 {
-                grid[line.start.1][i] += 1;
+                grid[line.start.1 as usize][i as usize] += 1;
             }
         }
     }
